9096a68fe78b216b4d9fae276fb0d32149f8e233
siddiquesk <mehnajk07@gmail.com>
2025-09-19 21:18:54 +0530

    ecommerce

--- backend/index.js
+++ backend/index.js
@@ -20,8 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 const corsOptions = {
   origin: [
-    "http://localhost:5174",
-    "http://localhost:5173",
     "https://ecommerce-luxurys.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
